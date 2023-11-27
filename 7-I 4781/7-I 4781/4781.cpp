29e3c36777c33e233030625891a706f17a90055d
KimJunSeo <108670965+k99812@users.noreply.github.com>
2023-11-27 17:06:57 +0900

    2023.11.27 Update

--- 7-I 4781/7-I 4781/4781.cpp
+++ 7-I 4781/7-I 4781/4781.cpp
@@ -1,10 +1,40 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 
 using namespace std;
 
+int n;
+float m;
+vector<pair<int, float>> v(5004);
+
+int go(int c, float p, int idx)
+{
+	//기저사례
+	if (p > m) return -1e9;
+	if (p == m) return 0;
+
+	//메모리제이션
+	int& ret;
+	if (ret) return ret;
+
+	//로직
+	return ret += max(go(c, p), )
+
+}
+
 int main()
 {
+	while (cin >> n && cin >> m)
+	{
+		if (m == 0.00f) break;
+
+		v.clear();
+
+		for (int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
+
+		
+	}
 
 	return 0;
 }
