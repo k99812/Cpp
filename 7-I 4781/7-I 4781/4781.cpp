c7609bd4bf095eff5ef77425a8e013af6ea0df21
KimJunSeo <108670965+k99812@users.noreply.github.com>
2023-11-28 15:43:40 +0900

    Update 4781.cpp

--- 7-I 4781/7-I 4781/4781.cpp
+++ 7-I 4781/7-I 4781/4781.cpp
@@ -5,27 +5,28 @@ using namespace std;
 
 int n, dp[10004], c, m1, m2;
 
-int main()
+int main() 
 {
-
 	while (true)
 	{
 		scanf_s("%d %d.%d", &n, &m1, &m2);
-		int cost = m1 * 100 + m2;
-		if (cost == 0) break;
+		if (n == 0) break;
 
 		fill(&dp[0], &dp[10004], 0);
+		int money = m1 * 100 + m2;
 
 		for (int i = 0; i < n; i++)
 		{
 			scanf_s("%d %d.%d", &c, &m1, &m2);
+
 			int price = m1 * 100 + m2;
-			for (int j = price; j <= cost; j++)
+
+			for (int j = price; j <= money; j++)
 			{
-				dp[j] = max(dp[j], dp[j - price] + c);
+				dp[j] = max(dp[j - price] + c, dp[j]);
 			}
 		}
-		cout << dp[cost] << "\n";
+		cout << dp[money] << "\n";
 	}
 
 	return 0;
