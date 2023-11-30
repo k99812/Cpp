fd3a5903cc0b01fac113a05fcae3d46591a59fab
KimJunSeo <108670965+k99812@users.noreply.github.com>
2023-11-30 16:40:40 +0900

    2023.11.30

--- 7-K 1513/7-K 1513/1513.cpp
+++ 7-K 1513/7-K 1513/1513.cpp
@@ -1,35 +1,52 @@
 #include<iostream>
 #include<algorithm>
-#include<vector>
 
 using namespace std;
 
-const int dy[] = { 1, 0 };
-const int dx[] = { 0, 1 };
-int n, m, c, a[54][54];
-vector<pair<int, int>> v(54);
+const int mod = 1000007;
+int n, m, c, a[54][54], dp[54][54][54][54], x, y;
 
 int go(int y, int x, int cnt, int prev)
 {
-	if (y == n && x == m) return 1;
-	
-	for (int i = 0; i < 2; i++)
+	if (y > n || x > m) return 0;
+	if (y == n && x == m)
 	{
-		int ny = y + dy[i], nx = x + dx[i];
-
-		if (ny < 1 || nx <1 || ny > n || nx > m) continue;
-		go(ny, nx, num);
+		if (cnt == 0 && a[y][x] == 0) return 1;
+		if (cnt == 1 && a[y][x] > prev) return 1;
+		return 0;
 	}
+
+	int& ret = dp[y][x][cnt][prev];
+	if (ret != -1) return ret;
+	ret = 0;
+
+	if (a[y][x] == 0)
+	{
+		ret = (go(y + 1, x, cnt, prev) + go(y, x + 1, cnt, prev)) % mod;
+		
+	}
+	else if(a[y][x] > prev)
+	{
+		ret = (go(y + 1, x, cnt - 1, a[y][x]) + go(y, x + 1, cnt - 1, a[y][x])) % mod;
+	}
+	return ret;
 }
 
 int main()
 {
 	cin >> n >> m >> c;
 
-	for (int i = 1; i <= c; i++) 
+	fill(&dp[0][0][0][0], &dp[53][53][53][54], -1);
+
+	for (int i = 1; i <= c; i++)
 	{ 
-		cin >> v[i].first >> v[i].second; 
-		a[v[i].first][v[i].second] = i;
+		cin >> y >> x;
+		a[y][x] = i;
+	}
+
+	for (int i = 0; i <= c; i++)
+	{
+		cout << go(1, 1, i, 0) << " ";
 	}
 
 	return 0;
