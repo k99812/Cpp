9b64cde6e4922da6fb81727bc403bf81d99e653c
KimJunSeo <108670965+k99812@users.noreply.github.com>
2023-12-19 15:44:36 +0900

    Update 2342.cpp

--- 7-W 2342/7-W 2342/2342.cpp
+++ 7-W 2342/7-W 2342/2342.cpp
@@ -1,11 +1,32 @@
 #include<iostream>
 #include<algorithm>
-#include<vector>
 
 using namespace std;
 
-int n;
-vector<int> v;
+int n, a[100004], dp[5][5][100004], num;
+
+int check(int from, int to)
+{
+	if (from == 0) return 2;
+
+	if (from == to) return 1;
+
+	if (abs(to - from) == 2) return 4;
+
+	return 3;
+}
+
+int go(int y, int x, int idx)
+{
+	if (idx == num) return 0;
+
+	int& ret = dp[y][x][idx];
+	if (ret) return ret;
+
+	int left = go(a[idx], x, idx + 1) + check(y, a[idx]);
+	int right = go(y, a[idx], idx + 1) + check(x, a[idx]);
+	return ret = min(left, right);
+}
 
 int main()
 {
@@ -13,8 +34,11 @@ int main()
 	{
 		if (n == 0) break;
 
-		v.push_back(n);
+		a[num] = n;
+		num++;
 	}
 
+	cout << go(0, 0, 0) << "\n";
+
 	return 0;
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
