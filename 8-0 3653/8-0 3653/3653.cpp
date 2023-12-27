9dce9f9f31898660fbc89d79e7d4c58ce5938cfa
KimJunSeo <108670965+k99812@users.noreply.github.com>
2023-12-27 17:18:54 +0900

    2023.12.27

--- 8-0 3653/8-0 3653/3653.cpp
+++ 8-0 3653/8-0 3653/3653.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
 #include<algorithm>
+#include<map>
 
 using namespace std;
 
-int t, n, m;
+int t, n, m, tree[200004], temp;
+map<int, int> mp;
+
+void update(int idx, int value)
+{
+	while (idx <= 200004)
+	{
+		tree[idx] += value;
+		idx += (idx & -idx);
+	}
+}
+
+int sum(int idx)
+{
+	int ret = 0;
+
+	while (idx > 0)
+	{
+		ret += tree[idx];
+		idx -= (idx & -idx);
+	}
+	return ret;
+}
 
 int main()
 {
@@ -12,6 +35,27 @@ int main()
 	while (t--)
 	{
 		cin >> n >> m;
+
+		fill(&tree[0], &tree[200004], 0);
+		mp.clear();
+		int update_idx = 100001;
+
+		for (int i = 1; i <= n; i++)
+		{
+			mp[i] = i + update_idx;
+			update(i + update_idx, 1);
+		}
+
+		for (int i = 0; i < m; i++)
+		{
+			cin >> temp;
+			int idx = mp[temp];
+			cout << sum(idx) - 1 << " ";
+			update(idx, -1);
+			update(--update_idx, 1);
+			mp[temp] = update_idx;
+		}
+		cout << "\n";
 	}
 
 	return 0;
