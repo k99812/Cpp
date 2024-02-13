cda6a7d751ec59b1b0da8b2248caea3ba5f930db
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-02-13 17:29:48 +0900

    2024.02.13

--- 8-P 1219/8-P 1219/1219.cpp
+++ 8-P 1219/8-P 1219/1219.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-const int INF = -1e9;
-int n, m, s, d, money[54], dist[54], x, y, z;
-vector<pair<int, int>> grap[54];
+const long long INF = 1e18;
+int n, m, s, d, vistied[104], x, y, z, isEnd;
+long long cost[104], dist[104];
+vector<pair<int, int>> grap[104];
+queue<int> q;
 
 int main()
 {
 	cin >> n >> s >> d >> m;
-
-	
 
 	for (int i = 0; i < m; i++)
 	{
@@ -24,8 +24,53 @@ int main()
 
 	for (int i = 0; i < n; i++)
 	{
-		cin >> money[i];
+		cin >> cost[i];     
 	}
+
+	fill(&dist[0], &dist[104], -INF);
+	dist[s] = cost[s];
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int here = 0; here < n; here++)
+		{
+			for (pair<int, int> p : grap[here])
+			{
+				int next_dist = p.first, next = p.second;
+
+				if (dist[here] != -INF && dist[here] + cost[next] - next_dist > dist[next])
+				{
+					dist[next] = dist[here] + cost[next] - next_dist;
+					if (i == n - 1) q.push(here);
+				}
+			}  
+		}
+	}
+
+	while (q.size())
+	{
+		int here = q.front(); q.pop();
+
+		for (pair<int, int> next : grap[here])
+		{
+			if (next.second == d)
+			{
+				isEnd = 1;
+				break;
+			}
+
+			if (!vistied[next.second])
+			{
+				vistied[next.second] = 1;
+				q.push(next.second);
+			}
+		}
+		if (isEnd) break;
+	}
+
+	if (dist[d] == -INF) cout << "gg" << "\n";
+	else if (isEnd) cout << "Gee" << "\n";
+	else cout << dist[d] << "\n";
 
 	return 0;
 }
