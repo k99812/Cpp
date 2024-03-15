2730eda0c5a80624c366e9fcb0113de098884ca1
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-03-15 20:31:32 +0900

    2024.03.15

--- 2024-03-15 9376/2024-03-15 9376/9376.cpp
+++ 2024-03-15 9376/2024-03-15 9376/9376.cpp
@@ -3,8 +3,9 @@
 #include<vector>
 #include<queue>
 #include<string>
+#include<string.h>
 
-#define INF 1e9
+#define INF 0xf
 
 using namespace std;
 
@@ -12,114 +13,97 @@ const int dy[] = { -1, 0, 1, 0 };
 const int dx[] = { 0, 1, 0, -1 };
 int t, h, w, dist[3][104][104];
 char m[104][104];
-string s;
-vector<pair<int, int>> sinner;
+pair<int, int> sinner[2];
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-void dijkstra(int idx,const pair<int, int>& start)
+void dijkstra(int idx, const pair<int, int>& start)
 {
-	dist[idx][start.first][start.second] = 0;
-	pq.push({ 0, start.first * 1000 + start.second });
+    dist[idx][start.first][start.second] = 0;
+    pq.push({ 0, start.first * 1000 + start.second });
 
-	while (pq.size())
-	{
-		int here_y = pq.top().second / 1000, here_x = pq.top().second % 1000;
-		int here_dist = pq.top().first; pq.pop();
+    while (pq.size())
+    {
+        int here_y = pq.top().second / 1000, here_x = pq.top().second % 1000;
+        int cost = pq.top().first;
+        pq.pop();
 
-		if (here_dist != dist[idx][here_y][here_x]) continue;
+        if (dist[idx][here_y][here_x] != cost) continue;
 
-		for (int i = 0; i < 4; i++)
-		{
-			int ny = here_y + dy[i], nx = here_x + dx[i];
-			int next_dist = m[ny][nx] == '#' ? 1 : 0;
+        for (int i = 0; i < 4; ++i)
+        {
+            int ny = here_y + dy[i], nx = here_x + dx[i];
+            int next_cost = cost;
 
-			if (ny < 0 || nx < 0 || ny > h + 1 || nx > w + 1) continue;
-			if (m[ny][nx] == '*') continue;
+            if (ny < 0 || nx < 0 || ny > h + 1 || nx > w + 1) continue;
 
-			if (dist[idx][ny][nx] > dist[idx][here_y][here_x] + next_dist)
-			{
-				dist[idx][ny][nx] = dist[idx][here_y][here_x] + next_dist;
-				pq.push({ dist[idx][ny][nx], ny * 1000 + nx });
-			}
-		}
-	}
-}
+            if (m[ny][nx] == '*') continue;
+            else if (m[ny][nx] == '#') next_cost++;
 
-void show()
-{
-	cout << "\n";
-	for (int k = 0; k < 3; k++)
-	{
-		for (int i = 0; i <= h + 1; i++)
-		{
-			for (int j = 0; j <= w + 1; j++)
-			{
-				if (m[i][j] == '*')
-				{
-					cout << "  ";
-					continue;
-				}
-				cout << dist[k][i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
+            if (dist[idx][ny][nx] > dist[idx][here_y][here_x] + next_cost)
+            {
+                pq.push({ next_cost, ny * 1000 + nx });
+                dist[idx][ny][nx] = next_cost;
+            }
+        }
+    }
+
 }
 
 int main()
 {
-	cin >> t;
+    cin >> t;
 
-	while (t--)
-	{
-		cin >> h >> w;
-		fill(&m[0][0], &m[103][104], '.');
-		fill(&dist[0][0][0], &dist[2][103][104], INF);
+    while (t--)
+    {
+        cin >> h >> w;
+        memset(dist, INF, sizeof(dist));
 
-		for (int i = 1; i <= h; i++)
-		{
-			cin >> s;
-			for(int j = 0; j < w; j++)
-			{
-				if (s[j] == '$')
-				{
-					sinner.push_back({ i, j + 1 });
-				}
-				m[i][j + 1] = s[j];
-			}
-		}
+        int order = 0;
+        string s;
+        for (int i = 1; i <= h; i++)
+        {
+            cin >> s;
+            s = '.' + s + '.';
+            for (int j = 0; j <= w + 1; j++)
+            {
+                m[i][j] = s[j];
+                if (m[i][j] == '$')
+                {
+                    sinner[order++] = { i,j };
+                }
+            }
+        }
 
-		dijkstra(0, {0, 0});
-		dijkstra(1, sinner[0]);
-		dijkstra(2, sinner[1]);
+        for (int i = 0; i <= w + 1; i++)
+        {
+            m[0][i] = '.';
+            m[h + 1][i] = '.';
+        }
 
-		//show();
+        dijkstra(0, { 0, 0 });
+        dijkstra(1, sinner[0]);
+        dijkstra(2, sinner[1]);
 
-		int minOpen = INF;
-		int openDoor;
+        int min_open_num = 1e9;
+        int opened_doors;
+        for (int i = 0; i <= h + 1; i++)
+        {
+            for (int j = 0; j <= w + 1; j++)
+            {
+                if (m[i][j] == '*')
+                    continue;
 
-		for (int i = 0; i <= h + 1; i++)
-		{
-			for (int j = 0; j <= w + 1; j++)
-			{
-				if (m[i][j] == '*')
-				{
-					cout << "  ";
-					continue;
-				}
+                opened_doors = dist[0][i][j] + dist[1][i][j] + dist[2][i][j];
+                if (m[i][j] == '#')
+                {
+                    opened_doors -= 2;
+                }
+                min_open_num = min(opened_doors, min_open_num);
+            }
+        }
 
-				openDoor = dist[0][i][j] + dist[1][i][j] + dist[2][i][j];
+        cout << min_open_num << '\n';
+    }
 
-				if (m[i][j] == '#')
-				{
-					openDoor -= 2;
-				}
-				minOpen = min(minOpen, openDoor);
-			}
-		}
-		
-		cout << minOpen << "\n";
-	}
-
-	return 0;
+    return 0;
 }
