a8795fa86b4bc003cfbad7e0de92e3c075ace196
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-06-11 17:12:23 +0900

    2024.06.11

--- 2024-06-07 11501/2024-06-07 11501/11501.cpp
+++ 2024-06-07 11501/2024-06-07 11501/11501.cpp
@@ -1,10 +1,35 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 
 using namespace std;
 
+int T, n, temp;
+
 int main()
 {
+	cin >> T;
+
+	while (T--)
+	{
+		cin >> n;
+		vector<int> v(n);
+
+		int max_num = 0;
+		long long ret = 0;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> v[i];
+		}
+
+		for (int i = n - 1; i >= 0; i--)
+		{
+			max_num = max(max_num, v[i]);
+			ret += max_num - v[i];
+		}
+
+		cout << ret << "\n";
+	}
 
 	return 0;
 }
