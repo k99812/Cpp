c6ed9bcc96d51074d811fbff59b7e43f8155bcdf
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-06-24 15:37:03 +0900

    2024.06.24

--- 2024-06-19 1937/2024-06-19 1937/1937.cpp
+++ 2024-06-19 1937/2024-06-19 1937/1937.cpp
@@ -1,11 +1,32 @@
 #include<iostream>
 #include<algorithm>
+#include<queue>
 
 #define MAX_NUM 500 + 4
 
 using namespace std;
 
-int n, a[MAX_NUM][MAX_NUM];
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
+int n, a[MAX_NUM][MAX_NUM], dp[MAX_NUM][MAX_NUM], num;
+
+int go(int y, int x)
+{
+	int& ret = dp[y][x];
+	if (ret) return ret;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i], nx = x + dx[i];
+
+		if (y < 0 || x < 0 || y >= n || x >= n) continue;
+		if (a[y][x] >= a[ny][nx]) continue;
+
+		ret = max(ret, go(ny, nx) + 1);
+	}
+
+	return ret;
+}
 
 int main()
 {
@@ -18,6 +39,16 @@ int main()
 			cin >> a[i][j];
 		}
 	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			num = max(num, go(i, j));
+		}
+	}
+
+	cout << num + 1 << "\n";
 
 	return 0;
 }
