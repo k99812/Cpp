49484b9fcd2110e648826f824b23896aa6a01ca9
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-07-25 16:15:31 +0900

    2024.07.25

--- 2024-07-24 11049/2024-07-24 11049/11049.cpp
+++ 2024-07-24 11049/2024-07-24 11049/11049.cpp
@@ -1,10 +1,44 @@
 #include<iostream>
 #include<algorithm>
 
+#define MAX_NUM 500 + 4
+#define INF 1e9
+
 using namespace std;
+
+int n, dp[MAX_NUM][MAX_NUM];
+pair<int, int> Matrix[MAX_NUM];
+
+int go(int y , int x)
+{
+	if (y == x)
+	{
+		return 0;
+	}
+
+	int& ret = dp[y][x];
+	if (ret != INF) return ret;
+
+	for (int i = y; i < x; i++)
+	{
+		ret = min(ret, go(y, i) + go(i + 1, x) + Matrix[y].first * Matrix[i].second * Matrix[x].second);
+	}
+
+	return ret;
+}
 
 int main()
 {
+	cin >> n;
+
+	fill(&dp[0][0], &dp[MAX_NUM - 1][MAX_NUM], INF);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> Matrix[i].first >> Matrix[i].second;
+	}
+
+	cout << go(0, n - 1) << "\n";
 
 	return 0;
 }
