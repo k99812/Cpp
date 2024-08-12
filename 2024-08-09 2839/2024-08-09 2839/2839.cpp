6f03009a3ba00e0401b687ac6a1e54006f403723
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-08-12 17:30:10 +0900

    2024.08.12

--- 2024-08-09 2839/2024-08-09 2839/2839.cpp
+++ 2024-08-09 2839/2024-08-09 2839/2839.cpp
@@ -1,13 +1,34 @@
 #include<iostream>
 #include<algorithm>
 
+#define MAX_NUM 5000 + 4 
+
 using namespace std;
 
-int n;
+int n, dp[MAX_NUM];
 
 int main()
 {
 	cin >> n;
+
+	int cnt = 0;
+
+	dp[3] = 1, dp[5] = 1;
+
+	for (int i = 6; i <= n; i++)
+	{
+		if (dp[i - 3])
+		{
+			dp[i] = dp[i - 3] + 1;
+		}
+
+		if (dp[i - 5])
+		{
+			dp[i] = dp[i] ? min(dp[i], dp[i - 5] + 1) : dp[i - 5] + 1;
+		}
+	}
+
+	cout << (dp[n] == 0 ? -1 : dp[n]) << "\n";
 
 	return 0;
 }
