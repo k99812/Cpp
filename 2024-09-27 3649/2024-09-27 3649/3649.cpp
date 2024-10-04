9aefe5d9f2fd7c3718639f17a1d5c625eb4e7393
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-10-04 17:12:09 +0900

    Update 3649.cpp

--- 2024-09-27 3649/2024-09-27 3649/3649.cpp
+++ 2024-09-27 3649/2024-09-27 3649/3649.cpp
@@ -1,10 +1,52 @@
 #include<iostream>
 #include<algorithm>
 
+#define MAX_NUM 1000000 + 4
+#define NANO 10000000
+
 using namespace std;
+
+int x, n, a[MAX_NUM];
 
 int main()
 {
+	while (cin >> x >> n)
+	{
+		fill(&a[0], &a[MAX_NUM], 0);
+		x *= NANO;
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+		}
+
+		sort(&a[0], &a[n]);
+
+		int left = 0, right = n - 1;
+		bool flag = false;
+
+		while (left < right)
+		{
+			int sum = a[left] + a[right];
+
+			if (sum == x)
+			{
+				flag = true;
+				break;
+			}
+
+			if (sum < x)
+			{
+				left++;
+			}
+			else if (sum > x)
+			{
+				right--;
+			}
+		}
+
+		flag ? cout << "yes " << a[left] << " " << a[right] << "\n" : cout << "danger\n";
+	}
 
 	return 0;
 }
