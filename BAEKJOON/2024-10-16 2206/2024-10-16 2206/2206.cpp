eeedd2bf901d1342f2815eceda354b668c95c392
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-10-16 16:19:30 +0900

    2024.10.16

--- BAEKJOON/2024-10-16 2206/2024-10-16 2206/2206.cpp
+++ BAEKJOON/2024-10-16 2206/2024-10-16 2206/2206.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<algorithm>
+#include<string>
 #include<queue>
 
 #define MAX_NUM 1000 + 4
+#define INF 1e9
 
 using namespace std;
 
@@ -13,9 +15,11 @@ struct Data
 		bWallIsDestroy;
 };
 
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
 int n, m, a[MAX_NUM][MAX_NUM], visited[MAX_NUM][MAX_NUM][2];
 
-void bfs()
+int bfs()
 {
 	queue<Data> q;
 	q.push({ 0,0,0 });
@@ -25,6 +29,35 @@ void bfs()
 	{
 		int y = q.front().y, x = q.front().x, bWallIsDestroy = q.front().bWallIsDestroy;
 		q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int ny = y + dy[i], nx = x + dx[i];
+
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+			if (visited[ny][nx][bWallIsDestroy] != INF) continue;
+			if (a[ny][nx] == 1)
+			{
+				if (bWallIsDestroy) continue;
+				else
+				{
+					int NextWall = 1;
+					visited[ny][nx][NextWall] = visited[y][x][bWallIsDestroy] + 1;
+					q.push({ ny, nx, NextWall });
+				}
+			}
+			else
+			{
+				visited[ny][nx][bWallIsDestroy] = visited[y][x][bWallIsDestroy] + 1;
+				q.push({ ny, nx, bWallIsDestroy });
+			}
+		}
+	}
+
+	if (visited[n - 1][m - 1][0] == INF && visited[n - 1][m - 1][1] == INF) return -1;
+	else
+	{
+		return min(visited[n - 1][m - 1][0], visited[n - 1][m - 1][1]);
 	}
 }
 
@@ -34,11 +67,17 @@ int main()
 
 	for (int i = 0; i < n; i++)
 	{
+		string s;
+		cin >> s;
 		for (int j = 0; j < m; j++)
 		{
-			cin >> a[i][j];
+			a[i][j] = s[j] - '0';
 		}
 	}
+
+	fill(&visited[0][0][0], &visited[MAX_NUM - 1][MAX_NUM - 1][2], INF);
+
+	cout << bfs() << "\n";
 
 	return 0;
 }
