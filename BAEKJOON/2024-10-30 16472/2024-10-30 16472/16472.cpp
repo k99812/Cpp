2bdb1cabe8c1a0a036a4283e7bccbadcb1c0d61d
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-10-30 17:06:01 +0900

    2024.10.30

---
+++ BAEKJOON/2024-10-30 16472/2024-10-30 16472/16472.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int n;
+string s;
+
+int main()
+{
+	cin >> n;
+	cin >> s;
+
+	int first = 0, second = 0;
+
+
+	return 0;
+}
