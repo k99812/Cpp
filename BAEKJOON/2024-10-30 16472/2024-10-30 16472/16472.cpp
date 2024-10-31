93fc90a6181c66ee471582d8a54544a36baf6528
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-10-31 16:42:39 +0900

    Update 16472.cpp

--- BAEKJOON/2024-10-30 16472/2024-10-30 16472/16472.cpp
+++ BAEKJOON/2024-10-30 16472/2024-10-30 16472/16472.cpp
@@ -1,19 +1,37 @@
 #include<iostream>
 #include<algorithm>
 #include<string>
+#include<map>
 
 using namespace std;
 
-int n;
+int n, ret = -1;
 string s;
 
 int main()
 {
 	cin >> n;
 	cin >> s;
+	
+	int first = 0, second = 0, cnt = 0;
+	map<char, int> m;
 
-	int first = 0, second = 0;
+	while (second < s.size())
+	{
+		if (!m[s[second]]) cnt++;
+		m[s[second]]++;
 
+		while (cnt > n)
+		{
+			if (--m[s[first]] == 0) cnt--;
+			first++;
+		}
+
+		ret = max(ret, second - first + 1);
+		second++;
+	}
+
+	cout << ret << "\n";
 
 	return 0;
 }
