2c8d25c15e8263b5f54e9a50e300c15d5fdfa389
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-12-19 17:10:09 +0900

    2024.12.19

--- 2024-12-18 14921/2024-12-18 14921/14921.cpp
+++ 2024-12-18 14921/2024-12-18 14921/14921.cpp
@@ -1,10 +1,45 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
+	int n;
+	cin >> n;
+	vector<int> v(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	int left = 0, right = n - 1, ret = 2e9;
+	pair<int, int> a;
+	while (left < right)
+	{
+		int sum = v[left] + v[right];
+
+		if (abs(sum) < ret)
+		{
+			ret = abs(sum);
+			a = { left, right };
+		}
+
+		if (sum <= 0)
+		{
+			left++;
+		}
+		else
+		{
+			right--;
+		}
+	}
+
+	cout << v[a.first] + v[a.second] << "\n";
 
 	return 0;
 }
