9ff533d2331344d67dd4497abc07efa8e8191a79
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-02-06 15:43:49 +0900

    2025.02.06

--- BAEKJOON/2025-02-06 12919/2025-02-06 12919/12919.cpp
+++ BAEKJOON/2025-02-06 12919/2025-02-06 12919/12919.cpp
@@ -1,10 +1,46 @@
 #include<iostream>
 #include<algorithm>
+#include<string>
 
 using namespace std;
 
+void go(const string& s, string& t)
+{
+	if (s == t)
+	{
+		cout << "1\n";
+		exit(0);
+	}
+
+	if (s.size() > t.size())
+	{
+		return;
+	}
+
+	if (t.back() == 'A')
+	{
+		string temp = t;
+		temp.pop_back();
+		go(s, temp);
+	}
+
+	if (t.front() == 'B')
+	{
+		string temp = t;
+		reverse(temp.begin(), temp.end());
+		temp.pop_back();
+		go(s, temp);
+	}
+}
+
 int main()
 {
+	string s, t;
+	cin >> s >> t;
+
+	go(s, t);
+
+	cout << "0\n";
 
 	return 0;
 }
