286d84ba2e8225ec2dc048346d0e85abe42ec29c
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-03-19 17:07:13 +0900

    2025.03.19

--- BAEKJOON/2025-03-18 1167/2025-03-18 1167/1167.cpp
+++ BAEKJOON/2025-03-18 1167/2025-03-18 1167/1167.cpp
@@ -1,20 +1,63 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
 int n;
 
+int dijkstra(int start,vector<int>& dist, const vector<vector<pair<int, int>>>& graph)
+{
+	dist.resize(n + 1, INT32_MAX);
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({ 0, start });
+	dist[start] = 0;
+
+	while (pq.size())
+	{
+		int now = pq.top().second, now_dist = pq.top().first;
+		pq.pop();
+
+		if (dist[now] != now_dist) continue;
+
+		for (pair<int, int> i : graph[now])
+		{
+			int next = i.second, next_dist = i.first;
+
+			if (dist[next] > dist[now] + next_dist)
+			{
+				dist[next] = dist[now] + next_dist;
+				pq.push({ dist[next], next });
+			}
+		}
+	}
+
+	int maxest = 0, num;
+	for (int i = 1; i <= n; i++)
+	{
+		if (dist[i] != INT32_MAX && dist[i] > maxest)
+		{
+			num = i;
+			maxest = dist[i];
+		}
+	}
+
+	return num;
+}
+
 int main()
 {
 	cin >> n;
 
-	vector<pair<int, int>> graph(n + 1);
+	vector<int> dist(n + 1, INT32_MAX);
+	vector<vector<pair<int, int>>> graph(n + 1);
 
-	for (int from = 1; from <= n; from++)
+	for (int i = 0; i < n; i++)
 	{
-		int to, dist;
+		int from, to, dist;
+
+		cin >> from;
 
 		while (cin >> to)
 		{
@@ -22,10 +65,17 @@ int main()
 
 			cin >> dist;
 
-			graph[from] = { dist, to };
-			graph[to] = { dist, from };
+			graph[from].push_back({ dist, to });
 		}
 	}
+
+	int num = dijkstra(1, dist, graph);
+
+	cout << num << " : " << dist[num] << "\n";
+
+	int ret = dijkstra(num, dist, graph);
+
+	cout << ret << " : " << dist[ret] << "\n";
 
 	return 0;
 }
