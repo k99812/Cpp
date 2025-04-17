081b2f7c6660412ec0254b4e8d1cd46b759df2bf
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-04-17 17:13:38 +0900

    2025.04.17

--- BAEKJOON/2025-04-16 14725/2025-04-16 14725/14725.cpp
+++ BAEKJOON/2025-04-16 14725/2025-04-16 14725/14725.cpp
@@ -1,26 +1,68 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<string>
+#include<map>
 
 using namespace std;
 
 int n;
 
+class Ant
+{
+private:
+	map<string, Ant*> child;
+
+public:
+	void insert(int idx, vector<string>& foods)
+	{
+		if (idx == foods.size())
+		{
+			return;
+		}
+
+		//map.find로 검색시 itr가 end가 되면 데이터가 없는것
+		if (child.find(foods[idx]) == child.end())
+		{
+			child[foods[idx]] = new Ant();
+		}
+		child[foods[idx]]->insert(idx + 1, foods);
+	}
+
+	void print(int idx)
+	{
+		for (const pair<string, Ant*>& c : child)
+		{
+			for (int i = 0; i < idx; i++)
+			{
+				cout << "--";
+			}
+			cout << c.first << "\n";
+			c.second->print(idx + 1);
+		}
+	}
+};
+
 int main()
 {
 	cin >> n;
 
+	Ant root;
 	while (n--)
 	{
 		int k;
 		cin >> k;
 
-		vector<char> v(k);
+		vector<string> v(k);
 		for (int i = 0; i < k; i++)
 		{
 			cin >> v[i];
 		}
+
+		root.insert(0, v);
 	}
+
+	root.print(0);
 
 	return 0;
 }
