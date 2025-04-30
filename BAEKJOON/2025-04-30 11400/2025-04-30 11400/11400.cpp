4bd5d5b9ed780882a271ad4cd01e45407042c7c5
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-04-30 17:12:21 +0900

    2025.04.30

---
+++ BAEKJOON/2025-04-30 11400/2025-04-30 11400/11400.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int v, e, cnt;
+vector<int> visited, parent;
+vector<vector<int>> graph;
+vector<pair<int, int>> bridge;
+
+int dfs(int start)
+{
+	visited[start] = ++cnt;
+	int ret = visited[start];
+
+	for (const int& next : graph[start])
+	{
+		if (parent[start] == next) continue;
+		if (visited[next])
+		{
+			ret = min(ret, visited[next]);
+			continue;
+		}
+		
+		parent[next] = start;
+		int low = dfs(next);
+		ret = min(ret, low);
+
+		if (low > visited[start])
+		{
+			bridge.push_back({ min(start, next), max(start, next) });
+		}
+	}
+
+	return ret;
+}
+
+int main()
+{
+	cin >> v >> e;
+
+	visited = parent = vector<int>(v + 1);
+	graph = vector<vector<int>>(v + 1);
+
+	for (int i = 0; i < e; i++)
+	{
+		int from, to;
+		cin >> from >> to;
+
+		graph[from].push_back(to);
+		graph[to].push_back(from);
+	}
+
+	for (int i = 1; i <= v; i++)
+	{
+		if (!visited[i])
+		{
+			dfs(i);
+		}
+	}
+
+	sort(bridge.begin(), bridge.end());
+
+	cout << bridge.size() << "\n";
+	for (const pair<int, int>& i : bridge)
+	{
+		cout << i.first << " " << i.second << "\n";
+	}
+
+	return 0;
+}
