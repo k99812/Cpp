dffd649ef43b61cdf2b8c513967ef18e4ea97e80
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-05-30 16:51:30 +0900

    2025.05.30

--- BAEKJOON/2025-05-29 1019/2025-05-29 1019/1019.cpp
+++ BAEKJOON/2025-05-29 1019/2025-05-29 1019/1019.cpp
@@ -4,13 +4,53 @@
 
 using namespace std;
 
-typedef long long ll;
+int n;
+vector<int> ret;
 
-ll n;
+vector<int> go(int n)
+{
+	vector<int> temp(10);
+
+	int pos = 1;
+	while (n / pos > 0)
+	{
+		int low = n % pos;
+		int now = (n / pos) % 10;
+		int high = n / (pos * 10);
+
+		for (int i = 0; i < 10; i++)
+		{
+			if (i < now)
+			{
+				temp[i] += (high + 1) * pos;
+			}
+			else if (i == now)
+			{
+				temp[i] += high * pos + low + 1;
+			}
+			else
+			{
+				temp[i] += high * pos;
+			}
+		}
+
+		temp[0] -= pos;
+		pos *= 10;
+	}
+
+	return temp;
+}
 
 int main()
 {
 	cin >> n;
+
+	ret = go(n);
+
+	for (const int& i : ret)
+	{
+		cout << i << " ";
+	}
 
 	return 0;
 }
