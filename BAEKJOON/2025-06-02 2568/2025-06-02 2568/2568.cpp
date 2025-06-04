279d02c61b27d11d03908ca81a3bd8ba38bea656
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-06-04 15:29:01 +0900

    2025.06.04

--- BAEKJOON/2025-06-02 2568/2025-06-02 2568/2568.cpp
+++ BAEKJOON/2025-06-02 2568/2025-06-02 2568/2568.cpp
@@ -1,23 +1,71 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<stack>
+#include<set>
 
 using namespace std;
 
-int n, len;
+int n;
 vector<pair<int, int>> v;
 vector<int> lis;
+stack<pair<int, int>> s;
 
 int main()
 {
 	cin >> n;
 
 	v = vector<pair<int, int>>(n);
-	lis = vector<int>(n);
 
 	for (int i = 0; i < n; i++)
 	{
 		cin >> v[i].first >> v[i].second;
+	}
+
+	sort(v.begin(), v.end());
+
+	for (const pair<int, int>& i : v)
+	{
+		vector<int>::iterator itr = lower_bound(lis.begin(), lis.end(), i.second);
+		int pos = itr - lis.begin();
+
+		if (itr == lis.end())
+		{
+			lis.push_back(i.second);
+		}
+		else
+		{
+			lis[pos] = i.second;
+		}
+
+		s.push({ pos, i.first });
+	}
+
+	int len = lis.size();
+	cout << n - len << "\n";
+	len--;
+
+	set<int> ret;
+	while (s.size())
+	{
+		int pos = s.top().first;
+		int val = s.top().second;
+
+		if (pos != len)
+		{
+			ret.insert(val);
+		}
+		else
+		{
+			len--;
+		}
+
+		s.pop();
+	}
+
+	for (const int& i : ret)
+	{
+		cout << i << "\n";
 	}
 
 	return 0;
