8fc20143f846fdae3eb762ef45247daac95e3f2f
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-06-11 16:52:31 +0900

    2025.06.11

--- BAEKJOON/2025-06-10 14942/2025-06-10 14942/14942.cpp
+++ BAEKJOON/2025-06-10 14942/2025-06-10 14942/14942.cpp
@@ -1,18 +1,64 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<cmath>
 
 using namespace std;
 
-int n;
-vector<int> ant;
+typedef long long ll;
+
+int n, LOG;
+vector<int> ant, depth;
 vector<vector<pair<int, int>>> graph;
+vector<vector<int>> parent;
+vector<vector<ll>> dist;
+
+void dfs(int idx, int par)
+{
+	for (const pair<int, int>& itr : graph[idx])
+	{
+		int next = itr.first, next_dist = itr.second;
+		if (next == par) continue;
+		depth[next] = depth[idx] + 1;
+		parent[next][0] = idx;
+		dist[next][0] = next_dist;
+		dfs(next, idx);
+	}
+}
+
+void build()
+{
+	for (int j = 1; j < LOG; j++)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			parent[i][j] = parent[parent[i][j - 1]][j - 1];
+			dist[i][j] = dist[i][j - 1] + dist[parent[i][j - 1]][j - 1];
+		}
+	}
+}
+
+int find(int node, ll energy)
+{
+	for (int j = LOG - 1; j >= 0; j--)
+	{
+		if (parent[node][j] && dist[node][j] <= energy)
+		{
+			energy -= dist[node][j];
+			node = parent[node][j];
+		}
+	}
+	return node;
+}
 
 int main()
 {
 	cin >> n;
-	ant = vector<int>(n + 1);
+	LOG = ceil(log2(n)) + 1;
+	ant = depth = vector<int>(n + 1);
 	graph = vector<vector<pair<int, int>>>(n + 1);
+	parent = vector<vector<int>>(n + 1, vector<int>(LOG));
+	dist = vector<vector<ll>>(n + 1, vector<ll>(LOG));
 
 	for (int i = 1; i <= n; i++)
 	{
@@ -26,6 +72,14 @@ int main()
 
 		graph[a].push_back({ b, dist });
 		graph[b].push_back({ a, dist });
+	}
+
+	dfs(1, 0);
+	build();
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << find(i, ant[i]) << "\n";
 	}
 
 	return 0;
