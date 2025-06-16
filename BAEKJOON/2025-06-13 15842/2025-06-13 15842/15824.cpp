692d54d05cb6b673f412587becf75163519e5066
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-06-16 17:11:48 +0900

    2025.06.16

--- BAEKJOON/2025-06-13 15842/2025-06-13 15842/15824.cpp
+++ BAEKJOON/2025-06-13 15842/2025-06-13 15842/15824.cpp
@@ -2,21 +2,43 @@
 #include<algorithm>
 #include<vector>
 
+#define MOD 1000000007
+
 using namespace std;
 
+typedef long long ll;
+
 int n;
-vector<int> v;
+ll ret;
+vector<ll> v, arr;
 
 int main()
 {
 	cin >> n;
 
-	v = vector<int>(n);
+	v = arr = vector<ll>(n);
 
-	for (int& i : v)
+	for (ll& i : v)
 	{
 		cin >> i;
 	}
+
+	sort(v.begin(), v.end());
+
+	arr[0] = 1;
+	for (int i = 1; i < n; i++)
+	{
+		arr[i] = (arr[i - 1] * 2) % MOD;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		ll maxNum = (v[i] * arr[i]) % MOD;
+		ll minNum = (v[i] * arr[n - 1 - i]) % MOD;
+		ret = (ret + maxNum - minNum + MOD) % MOD;
+	}
+
+	cout << ret << "\n";
 
 	return 0;
 }
