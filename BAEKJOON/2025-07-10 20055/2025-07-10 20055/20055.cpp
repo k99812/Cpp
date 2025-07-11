762c6ccf885e234b7f315c471c7e69df2800713c
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-07-11 17:11:00 +0900

    2025.07.11

--- BAEKJOON/2025-07-10 20055/2025-07-10 20055/20055.cpp
+++ BAEKJOON/2025-07-10 20055/2025-07-10 20055/20055.cpp
@@ -5,18 +5,51 @@
 using namespace std;
 
 int n, k;
-vector<int> v;
+vector<pair<int, int>> v;
+
+int go()
+{
+	int step = 0, broken = 0;
+	while (broken < k)
+	{
+		rotate(v.begin(), v.end() - 1, v.end());
+		v[n - 1].second = false;
+
+		for (int i = n - 2; i >= 0; i--)
+		{
+			if (v[i].second && !v[i + 1].second && v[i + 1].first > 0)
+			{
+				v[i].second = false;
+				v[i + 1].second = true;
+				if (--v[i + 1].first == 0) ++broken;
+			}
+		}
+		v[n - 1].second = false;
+
+		if (v[0].first > 0 && !v[0].second)
+		{
+			if (--v[0].first == 0) ++broken;
+			v[0].second = true;
+		}
+
+		++step;
+	}
+
+	return step;
+}
 
 int main()
 {
 	cin >> n >> k;
 
-	v = vector<int>(2 * n);
+	v = vector<pair<int, int>>(2 * n);
 
 	for (int i = 0; i < 2 * n; i++)
 	{
-		cin >> v[i];
+		cin >> v[i].first;
 	}
+
+	cout << go() << "\n";
 
 	return 0;
 }
