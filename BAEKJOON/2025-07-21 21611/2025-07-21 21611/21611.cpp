91ab2608821419f72458d72dad609f9d01c386d8
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-07-22 17:23:29 +0900

    2025.07.22

--- BAEKJOON/2025-07-21 21611/2025-07-21 21611/21611.cpp
+++ BAEKJOON/2025-07-21 21611/2025-07-21 21611/21611.cpp
@@ -1,12 +1,107 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<unordered_map>
 
 using namespace std;
+
+const int dy[2][4] = { { -1, 1, 0, 0 }, { 0, 1, 0, -1} };
+const int dx[2][4] = { { 0, 0, -1, 1 }, { -1, 0, 1, 0} };
 
 int n, m;
 vector<vector<int>> board;
 pair<int, int> shark;
+unordered_map<int, int> ret;
+
+void move(vector<vector<int>>& temp)
+{
+
+}
+
+bool destroy()
+{
+	int y = shark.first, x = shark.second;
+	int cnt = 0, s = 1, dir = 0, num_cnt = 1;
+
+	vector<vector<int>> temp = board;
+	vector<pair<int, int>> cordi;
+
+	while (true)
+	{
+		if ((y == 0 && x == 0) || temp[y][x] == 0) break;
+
+		if (cnt != 0 && cnt % 2 == 0) ++s;
+
+		for (int step = 1; step <= s; step++)
+		{
+			int ny = y + dy[1][dir], nx = x + dx[1][dir];
+
+			if (ny < 0 || nx < 0 || ny >= n || nx >= n) break;
+			if (temp[ny][nx] == 0)
+			{
+				y = ny, x = nx;
+				break;
+			}
+
+			if (temp[y][x] != temp[ny][nx])
+			{
+				if (num_cnt >= 4)
+				{
+					for (const pair<int, int>& itr : cordi)
+					{
+						temp[itr.first][itr.second] = 0;
+					}
+
+					move(temp);
+
+					y = shark.first, x = shark.second;
+					cnt = 0, s = 1, dir = 0;
+					board = temp;
+				}
+
+				if(cordi.size()) cordi.clear();
+				num_cnt = 1;
+			}
+			else
+			{
+				cordi.push_back({ ny, nx });
+				++num_cnt;
+			}
+
+			y = ny, x = nx;
+		}
+
+		++cnt;
+		dir = (dir + 1) % 4;
+	}
+
+	board = temp;
+}
+
+void go(int d, int s)
+{
+	int y = shark.first, x = shark.second;
+	vector<vector<int>> temp = board;
+
+	for (int i = 1; i <= s; i++)
+	{
+		int ny = y + dy[0][d] * i, nx = x + dx[0][d] * i;
+
+		if (ny < 0 || nx < 0 || ny >= n || nx >= n) break;
+		if (temp[ny][nx] == 0) continue;
+
+		temp[ny][nx] = 0;
+	}
+
+	move(temp);
+	board = temp;
+
+	while (true)
+	{
+		bool flag = destroy();
+		if (!flag) break;
+	}
+}
 
 int main()
 {
@@ -14,6 +109,7 @@ int main()
 
 	shark = { (n + 1) / 2, (n + 1) / 2 };
 	board = vector<vector<int>>(n, vector<int>(n));
+	ret[1] = 0, ret[2] = 0, ret[3] = 0;
 
 	for (int i = 0; i < n; i++)
 	{
@@ -27,6 +123,8 @@ int main()
 	{
 		int d, s;
 		cin >> d >> s;
+
+		go(--d, s);
 	}
 	
 	return 0;
---
+++ BAEKJOON/2025-07-22 13458/2025-07-22 13458/132458.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int n, b, c;
+vector<int> nums;
+
+int main()
+{
+	cin >> n;
+
+	nums = vector<int>(n);
+
+	for (int& i : nums)
+	{
+		cin >> i;
+	}
+
+	cin >> b >> c;
+
+	long long ret = 0;
+	for (const int& num : nums)
+	{
+		long long temp = num - b;
+		ret++;
+
+		if (temp > 0)
+		{
+			ret += temp / c;
+			if (temp % c != 0) ret++;
+		}
+	}
+
+	cout << ret << "\n";
+
+	return 0;
+}
