831b85f1dbfc0c9f6058810bfe85fdbee153f626
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-08-12 15:21:39 +0900

    2025.08.12

--- BAEKJOON/2025-08-11 7453/2025-08-11 7453/7453.cpp
+++ BAEKJOON/2025-08-11 7453/2025-08-11 7453/7453.cpp
@@ -19,5 +19,54 @@ int main()
 		cin >> a[i] >> b[i] >> c[i] >> d[i];
 	}
 
+	vector<int> ab_sum(n * n), cd_sum(n * n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			ab_sum[i * n + j] = a[i] + b[j];
+			cd_sum[i * n + j] = c[i] + d[j];
+		}
+	}
+
+	sort(ab_sum.begin(), ab_sum.end());
+	sort(cd_sum.begin(), cd_sum.end());
+
+	long long ret = 0;
+	int left = 0, right = cd_sum.size() - 1;
+
+	while (left < ab_sum.size() && right >= 0)
+	{
+		int mid = ab_sum[left] + cd_sum[right];
+		if (mid == 0)
+		{
+			int ab = ab_sum[left], cd = cd_sum[right];
+			long long ab_cnt = 0, cd_cnt = 0;
+
+			while (left < ab_sum.size() && ab == ab_sum[left])
+			{
+				ab_cnt++, left++;
+			}
+
+			while (right >= 0 && cd == cd_sum[right])
+			{
+				cd_cnt++, right--;
+			}
+
+			ret += ab_cnt * cd_cnt;
+		}
+		else if (mid < 0)
+		{
+			left++;
+		}
+		else
+		{
+			right--;
+		}
+	}
+
+	cout << ret << "\n";
+
 	return 0;
 }
