90d18b3b9104750622f5e33f41607dc65e625c6e
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-08-14 16:02:20 +0900

    2025.08.14

--- BAEKJOON/2025-08-13 20366/2025-08-13 20366/20366.cpp
+++ BAEKJOON/2025-08-13 20366/2025-08-13 20366/20366.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+struct Info
+{
+	int sum, first, second;
+
+	bool operator<(const Info& other) const
+	{
+		return sum < other.sum;
+	}
+};
+
 int n;
 vector<int> arr;
 
@@ -17,6 +27,38 @@ int main()
 	{
 		cin >> arr[i];
 	}
+
+	vector<Info> sums;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			sums.push_back({ arr[i] + arr[j], i, j });
+		}
+	}
+
+	sort(sums.begin(), sums.end());
+
+	int min_num = INT32_MAX;
+
+	for (int i = 0; i < sums.size(); i++)
+	{
+		for (int j = i + 1; j < sums.size(); j++)
+		{
+			Info& snowman_1 = sums[i];
+			Info& snowman_2 = sums[j];
+
+			if (snowman_1.first != snowman_2.first && snowman_1.first != snowman_2.second &&
+				snowman_1.second != snowman_2.first && snowman_1.second != snowman_2.second)
+			{
+				min_num = min(min_num, snowman_2.sum - snowman_1.sum);
+
+				break;
+			}
+		}
+	}
+
+	cout << min_num << "\n";
 
 	return 0;
 }
