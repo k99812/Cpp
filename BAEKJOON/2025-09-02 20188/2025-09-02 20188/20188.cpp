391d8d28bfa6dae00c1ed56490101138ef850dbf
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-09-08 15:07:23 +0900

    2025.09.08

--- BAEKJOON/2025-09-02 20188/2025-09-02 20188/20188.cpp
+++ BAEKJOON/2025-09-02 20188/2025-09-02 20188/20188.cpp
@@ -4,22 +4,47 @@
 
 using namespace std;
 
+typedef long long ll;
+
 int n;
 vector<vector<int>> graph;
+vector<int> subtree;
+ll ret;
+
+int dfs(int now, int parent)
+{
+	subtree[now] = 1;
+
+	for (const int& next : graph[now])
+	{
+		if (next == parent) continue;
+		
+		int cnt = dfs(next, now);
+		ll out = n - cnt;
+		ret += (ll)n * (n - 1) / 2 - out * (out - 1) / 2;
+		subtree[now] += cnt;
+	}
+
+	return subtree[now];
+}
 
 int main()
 {
 	cin >> n;
 
 	graph = vector<vector<int>>(n + 1);
+	subtree = vector<int>(n + 1);
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n - 1; i++)
 	{
 		int a, b;
 		cin >> a >> b;
 		graph[a].push_back(b);
 		graph[b].push_back(a);
 	}
+
+	dfs(1, 0);
+	cout << ret << "\n";
 
 	return 0;
 }
