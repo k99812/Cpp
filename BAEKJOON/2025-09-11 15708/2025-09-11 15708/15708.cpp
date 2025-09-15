ec08075bbe7526f8f41f58e4a9deddb84b24868f
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-09-15 17:11:35 +0900

    2025.09.15

--- BAEKJOON/2025-09-11 15708/2025-09-11 15708/15708.cpp
+++ BAEKJOON/2025-09-11 15708/2025-09-11 15708/15708.cpp
@@ -1,21 +1,47 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
-int n, t, p;
-vector<int> rock;
+typedef long long ll;
+
+ll n, t, p;
 
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
 	cin >> n >> t >> p;
-	rock = vector<int>(n);
+
+	ll sum = 0;
+	int cnt = 0, ret = 0;
+	priority_queue<int> pq;
 
 	for (int i = 0; i < n; i++)
 	{
-		cin >> rock[i];
+		if (t - p * i < 0) break;
+
+		int k;
+		cin >> k;
+
+		sum += k;
+		pq.push(k);
+		cnt++;
+
+		while (sum + p * i > t)
+		{
+			sum -= pq.top();
+			pq.pop();
+			cnt--;
+		}
+
+		ret = max(ret, cnt);
 	}
 
+	cout << ret << "\n";
+	
 	return 0;
 }
