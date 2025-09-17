478499d1753bd9dbff92c33811923b5b6e45e968
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-09-17 15:52:23 +0900

    2025.09.17

--- BAEKJOON/2025-09-16 14572/2025-09-16 14572/14572.cpp
+++ BAEKJOON/2025-09-16 14572/2025-09-16 14572/14572.cpp
@@ -1,35 +1,80 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
-#include<unordered_map>
 
 using namespace std;
 
+typedef long long ll;
+
+struct Info
+{
+	int skill;
+	vector<int> algo;
+};
+
 int n, k, d;
-vector<pair<int, int>> student;
-unordered_map<int, vector<int>> algo;
+vector<Info> student;
 
 int main()
 {
 	cin >> n >> k >> d;
 
-	student = vector<pair<int, int>>(n);
+	student = vector<Info>(n);
 
 	for (int i = 0; i < n; i++)
 	{
 		int m, s;
 		cin >> m >> s;
 
-		student[i] = { m, s };
-		vector<int> temp(m);
+		student[i].skill = s;
+		student[i].algo.resize(m);
 
 		for (int j = 0; j < m; j++)
 		{
-			cin >> temp[j];
+			cin >> student[i].algo[j];
+		}
+	}
+
+	sort(student.begin(), student.end(),
+		[](const Info& a, const Info& b)
+	{
+		return a.skill < b.skill;
+	});
+
+	int left = 0;
+	ll ret = 0;
+	vector<int> know_algo_cnt(k + 1);
+
+	for (int right = 0; right < n; right++)
+	{
+		for (const int& algo_num : student[right].algo)
+		{
+			know_algo_cnt[algo_num]++;
 		}
 
-		algo[i] = temp;
+		while (student[right].skill - student[left].skill > d)
+		{
+			for (const int& algo_num : student[left].algo)
+			{
+				know_algo_cnt[algo_num]--;
+			}
+
+			left++;
+		}
+
+		int group_size = right - left + 1, total = 0, all_know = 0;
+
+		for (int i = 1; i <= k; i++)
+		{
+			if (know_algo_cnt[i]) total++;
+			if (know_algo_cnt[i] == group_size) all_know++;
+		}
+
+		ll effi = 1LL * (total - all_know) * group_size;
+		ret = max(ret, effi);
 	}
+		
+	cout << ret << "\n";
 
 	return 0;
 }
