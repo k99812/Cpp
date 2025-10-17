234bf3c094edb1aa9892418719f1ba538faa4db2
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-10-17 15:37:15 +0900

    2025.10.17

---
+++ BAEKJOON/2025-10-17 2304/2025-10-17 2304/2304.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+struct Cordi
+{
+	int y, x;
+
+	bool operator<(const Cordi& other) const
+	{
+		if (x != other.x) return x < other.x;
+		return y < other.y;
+	}
+};
+
+int n;
+vector<Cordi> arr;
+
+int main()
+{
+	cin >> n;
+
+	arr = vector<Cordi>(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i].x >> arr[i].y;
+	}
+
+	return 0;
+}
