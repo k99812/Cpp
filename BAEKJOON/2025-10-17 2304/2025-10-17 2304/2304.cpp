3228ea7a5476f62e102b5a133c6a8e3774998802
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-10-21 15:23:12 +0900

    2025.10.21

--- BAEKJOON/2025-10-17 2304/2025-10-17 2304/2304.cpp
+++ BAEKJOON/2025-10-17 2304/2025-10-17 2304/2304.cpp
@@ -10,8 +10,7 @@ struct Cordi
 
 	bool operator<(const Cordi& other) const
 	{
-		if (x != other.x) return x < other.x;
-		return y > other.y;
+		return x < other.x;
 	}
 };
 
@@ -31,7 +30,44 @@ int main()
 
 	sort(arr.begin(), arr.end());
 
+	int max_h = 0, max_idx = 0;
 
+	for (int i = 0; i < n; i++)
+	{
+		if (max_h < arr[i].y)
+		{
+			max_h = arr[i].y;
+			max_idx = i;
+ 		}
+	}
+
+	int ret = 0, now_h = 0;
+
+	for (int i = 0; i < max_idx; i++)
+	{
+		if (now_h < arr[i].y)
+		{
+			now_h = arr[i].y;
+		}
+
+		ret += (arr[i + 1].x - arr[i].x) * now_h;
+	}
+
+	now_h = 0;
+
+	for (int i = n - 1; i > max_idx; i--)
+	{
+		if (now_h < arr[i].y)
+		{
+			now_h = arr[i].y;
+		}
+
+		ret += (arr[i].x - arr[i - 1].x) * now_h;
+	}
+
+	ret += max_h;
+
+	cout << ret << "\n";
 
 	return 0;
 }
