3123d69807f3c4824aaface1afc5fb97655c52c4
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-11-12 17:10:37 +0900

    2025.11.12

---
+++ BAEKJOON/2025-11-12 16120/2025-11-12 16120/16120.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+const string P = "PPAP";
+string str;
+
+int main()
+{
+	cin >> str;
+
+	return 0;
+}
