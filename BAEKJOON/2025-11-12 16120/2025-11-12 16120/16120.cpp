f682bed0bb86af8e9964cc4da56e3682225e3341
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-11-13 15:57:12 +0900

    2025.11.13

--- BAEKJOON/2025-11-12 16120/2025-11-12 16120/16120.cpp
+++ BAEKJOON/2025-11-12 16120/2025-11-12 16120/16120.cpp
@@ -5,12 +5,40 @@
 
 using namespace std;
 
-const string P = "PPAP";
 string str;
 
 int main()
 {
 	cin >> str;
+
+	vector<char> alpha;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == 'P')
+		{
+			alpha.push_back(str[i]);
+			continue;
+		}
+
+		if (alpha.size() < 2 || i + 1 >= str.length() ||str[i + 1] != 'P')
+		{
+			cout << "NP" << "\n";
+			return 0;
+		}
+
+		alpha.pop_back();
+		i++;
+	}
+
+	if (alpha.size() == 1 && alpha[0] == 'P')
+	{
+		cout << "PPAP" << "\n";
+	}
+	else
+	{
+		cout << "NP" << "\n";
+	}
 
 	return 0;
 }
