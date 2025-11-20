ddb91b463c05ed2b8ce063b5bc1613d1d4ee1ece
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-11-20 16:27:53 +0900

    2025.11.20

--- BAEKJOON/2025-11-19 1552/2025-11-19 1552/1552.cpp
+++ BAEKJOON/2025-11-19 1552/2025-11-19 1552/1552.cpp
@@ -5,14 +5,69 @@
 
 using namespace std;
 
-int n;
-vector<vector<char>> board;
+const int INF = 2e9;
+
+int n, max_num = -INF, min_num = INF;
+vector<vector<int>> board;
+vector<bool> col;
+vector<int> selection;
+
+int find_cycle()
+{
+	int cycle = 0;
+	vector<bool> visited(n, false);
+
+	for (int i = 0; i < n; i++)
+	{
+		if (visited[i]) continue;
+
+		cycle++;
+		int cur = i;
+		while (!visited[cur])
+		{
+			visited[cur] = true;
+			cur = selection[cur];
+		}
+	}
+
+	return cycle;
+}
+
+void dfs(int r)
+{
+	if (r == n)
+	{
+		int point = 1;
+		for (int i = 0; i < n; i++) point *= board[i][selection[i]];
+
+		int cycle = find_cycle();
+		if (cycle % 2 == 0) point *= -1;
+
+		max_num = max(max_num, point);
+		min_num = min(min_num, point);
+		return;
+	}
+
+	for (int c = 0; c < n; c++)
+	{
+		if (col[c]) continue;
+		
+		col[c] = true;
+		selection[r] = c;
+
+		dfs(r + 1);
+
+		col[c] = false;
+	}
+}
 
 int main()
 {
 	cin >> n;
 
-	board = vector<vector<char>>(n, vector<char>(n));
+	board = vector<vector<int>>(n, vector<int>(n));
+	selection = vector<int>(n);
+	col = vector<bool>(n);
 
 	for (int i = 0; i < n; i++)
 	{
@@ -20,9 +75,20 @@ int main()
 		cin >> s;
 		for (int j = 0; j < n; j++)
 		{
-			board[i][j] = s[j];
+			if(isdigit(s[j]))
+			{
+				board[i][j] = s[j] - '0';
+			}
+			else
+			{
+				board[i][j] = -(s[j] - 'A' + 1);
+			}
 		}
 	}
+
+	dfs(0);
+
+	cout << min_num << "\n" << max_num << "\n";
 
 	return 0;
 }
