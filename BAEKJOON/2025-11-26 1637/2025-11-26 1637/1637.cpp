e9fa9334923737999ccebc5395bb2cc7df0cc9b8
KimJunSeo <108670965+k99812@users.noreply.github.com>
2025-11-27 15:56:35 +0900

    2025.11.27

--- BAEKJOON/2025-11-26 1637/2025-11-26 1637/1637.cpp
+++ BAEKJOON/2025-11-26 1637/2025-11-26 1637/1637.cpp
@@ -11,8 +11,24 @@ struct Info
 	ll a, b, c;
 };
 
+const ll MAX = 2147483647;
 int n;
 vector<Info> arr;
+
+ll getSum(const ll& num)
+{
+	ll sum = 0;
+	for (const Info& info : arr)
+	{
+		if (info.a > num) continue;
+
+		ll end = min(num, info.c);
+
+		sum += (end - info.a) / info.b + 1;
+	}
+
+	return sum;
+}
 
 int main()
 {
@@ -24,6 +40,25 @@ int main()
 	{
 		cin >> arr[i].a >> arr[i].c >> arr[i].b;
 	}
+
+	ll left = 1, right = MAX, ret = -1;
+	while (left <= right)
+	{
+		ll mid = (left + right) / 2;
+
+		if (getSum(mid) %2 != 0)
+		{
+			ret = mid;
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+
+	if (ret == -1) cout << "NOTHING" << "\n";
+	else cout << ret << " " << getSum(ret) - getSum(ret - 1) << "\n";
 	
 	return 0;
 }
