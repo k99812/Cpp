de122077cdafe1f7639d10c1d235fa2ee11996ce
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-01-19 17:22:23 +0900

    2026.01.19

---
+++ BAEKJOON/2026-01-19 1867/2026-01-19 1867/1867.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int n, m;
+
+vector<vector<int>> graph;
+vector<int> match, visited;
+
+bool dfs(int now)
+{
+	for (const int next : graph[now])
+	{
+		if (visited[next]) continue;
+
+		visited[next] = true;
+
+		if (match[next] == -1 || dfs(match[next]))
+		{
+			match[next] = now;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int main()
+{
+	cin >> n >> m;
+
+	graph.assign(n + 1, vector<int>());
+	match.assign(n + 1, -1);
+
+	for (int i = 0; i < m; i++)
+	{
+		int y, x;
+		cin >> y >> x;	
+		graph[y].push_back(x);
+	}
+
+	int match_cnt = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		visited.assign(n + 1, false);
+
+		if (dfs(i))
+		{
+			match_cnt++;
+		}
+	}
+
+	cout << match_cnt << "\n";
+
+	return 0;
+}
