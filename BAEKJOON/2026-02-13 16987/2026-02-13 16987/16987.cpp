ede54f387f50455e6a19d3ac99648843508acaff
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-02-19 17:23:29 +0900

    2026.02.19

--- BAEKJOON/2026-02-13 16987/2026-02-13 16987/16987.cpp
+++ BAEKJOON/2026-02-13 16987/2026-02-13 16987/16987.cpp
@@ -4,8 +4,47 @@
 
 using namespace std;
 
-int n;
+int n, ret = -1;
 vector<pair<int, int>> arr;
+
+void go(int now)
+{
+	if (now == n)
+	{
+		int cnt = 0;
+		for (const pair<int, int>& p : arr)
+		{
+			if (p.first <= 0) cnt++;
+		}
+
+		ret = max(ret, cnt);
+
+		return;
+	}
+
+	if (arr[now].first <= 0)
+	{
+		go(now + 1);
+		return;
+	}
+
+	bool flag = false;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (i == now || arr[i].first <= 0) continue;
+
+		flag = true;
+
+		arr[now].first -= arr[i].second;
+		arr[i].first -= arr[now].second;
+		go(now + 1);
+		arr[now].first += arr[i].second;
+		arr[i].first += arr[now].second;
+	}
+
+	if (!flag) go(now + 1);
+}
 
 int main()
 {
@@ -17,6 +56,10 @@ int main()
 	{
 		cin >> arr[i].first >> arr[i].second;
 	}
+
+	go(0);
+
+	cout << ret << "\n";
 
 	return 0;
 }
