df7982b94410e9a19c64cab17fdc90cf4ac18471
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-03-03 17:11:30 +0900

    2026.03.03

--- BAEKJOON/2026-02-27 2186/2026-02-27 2186/2186.cpp
+++ BAEKJOON/2026-02-27 2186/2026-02-27 2186/2186.cpp
@@ -5,20 +5,67 @@
 
 using namespace std;
 
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
+
 int n, m, k;
 vector<string> arr;
-string str;
+vector<vector<vector<int>>> dp;
+string target;
+
+int dfs(int idx, int y, int x)
+{
+	if (idx == target.size() - 1) return 1;
+
+	int& ret = dp[y][x][idx];
+	if (ret != -1) return ret;
+
+	ret = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 1; j <= k; j++)
+		{
+			int ny = y + dy[i] * j, nx = x + dx[i] * j;
+
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+			if (arr[ny][nx] != target[idx + 1]) continue;
+
+			ret += dfs(idx + 1, ny, nx);
+		}
+	}
+
+	return ret;
+}
 
 int main()
 {
 	cin >> n >> m >> k;
+
+	arr.assign(n, "");
 
 	for (int i = 0; i < n; i++)
 	{
 		cin >> arr[i];
 	}
 
-	cin >> str;
+	cin >> target;
+
+	dp.assign(n, vector<vector<int>>(m, vector<int>(target.size(), -1)));
+
+	int ret = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (arr[i][j] == target[0])
+			{
+				ret += dfs(0, i, j);
+			}
+		}
+	}
+
+	cout << ret << "\n";
 
 	return 0;
 }
