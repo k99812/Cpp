5fedd6cf3873d3377cff19c628c1ab75e725ce69
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-04-15 17:00:42 +0900

    2026.04.15

--- BAEKJOON/2026-04-14 18809/2026-04-14 18809/18809.cpp
+++ BAEKJOON/2026-04-14 18809/2026-04-14 18809/18809.cpp
@@ -4,8 +4,20 @@
 
 using namespace std;
 
+int n, m, g, r;
+vector<vector<int>> board;
+
 int main()
 {
+	cin >> n >> m >> g >> r;
+
+	for(int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> board[i][j];
+		}
+	}
 
 	return 0;
 }
