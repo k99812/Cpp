46a065723bec3f25ef7c6bc4cbee98f44eca908f
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-04-20 16:54:47 +0900

    2026.04.20

--- BAEKJOON/2026-04-17 2283/2026-04-17 2283/2283.cpp
+++ BAEKJOON/2026-04-17 2283/2026-04-17 2283/2283.cpp
@@ -4,11 +4,56 @@
 
 using namespace std;
 
+const int MAX_POS = 1000000;
+
 int n, k;
+vector<int> arr;
 
 int main()
 {
 	cin >> n >> k;
+
+	arr.assign(MAX_POS + 4, 0);
+
+	int max_r = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int l, r;
+		cin >> l >> r;
+		max_r = max(max_r, r);
+
+		arr[l]++;
+		arr[r]--;
+	}
+
+	for (int i = 1; i <= max_r; i++)
+	{
+		arr[i] += arr[i - 1];
+	}
+
+	int left = 0, right = 0;
+	long long sum = 0;
+
+	while (right <= max_r || sum >= k)
+	{
+		if (sum == k)
+		{
+			cout << left << " " << right << "\n";
+			return 0;
+		}
+		else if (sum < k)
+		{
+			sum += arr[right];
+			right++;
+		}
+		else
+		{
+			sum -= arr[left];
+			left++;
+		}
+	}
+
+	cout << 0 << " " << 0 << "\n";
 
 	return 0;
 }
