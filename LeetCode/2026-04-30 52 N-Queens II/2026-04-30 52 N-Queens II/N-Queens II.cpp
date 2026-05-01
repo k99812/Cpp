ff70326eb027834ca3b91ef31a5f5d236146d331
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-05-01 16:42:22 +0900

    2026.05.01

--- LeetCode/2026-04-30 52 N-Queens II/2026-04-30 52 N-Queens II/N-Queens II.cpp
+++ LeetCode/2026-04-30 52 N-Queens II/2026-04-30 52 N-Queens II/N-Queens II.cpp
@@ -6,14 +6,49 @@ using namespace std;
 
 class Solution 
 {
-	int n;
+	int n, ret = 0;
+	vector<bool> col, right_cross, left_cross;
+
+	void dfs(int r)
+	{
+		if (r == n)
+		{
+			ret++;
+			return;
+		}
+
+		for (int c = 0; c < n; c++)
+		{
+			if (col[c] || right_cross[r + c] || left_cross[r - c + n - 1]) continue;
+
+			col[c] = true;
+			right_cross[r + c] = true, left_cross[r - c + n - 1] = true;
+
+			dfs(r + 1);
+
+			col[c] = false;
+			right_cross[r + c] = false, left_cross[r - c + n - 1] = false;
+		}
+	}
 
 public:
+	Solution() : n(0)
+	{
+
+	}
+
 	int totalNQueens(int num) 
 	{
 		n = num;
+		ret = 0;
 		
-		return 0;
+		col.assign(n, false);
+		right_cross.assign(n + n - 1, false);
+		left_cross.assign(n + n - 1, false);
+
+		dfs(0);
+
+		return ret;
 	}
 };
 
