09d2e9c013a69c8d7f91b88044d7eca03a564f6d
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-06-02 17:12:22 +0900

    2026.06.02

--- LeetCode/2026-05-29 127. Word Ladder/2026-05-29 127. Word Ladder/127. Word Ladder.cpp
+++ LeetCode/2026-05-29 127. Word Ladder/2026-05-29 127. Word Ladder/127. Word Ladder.cpp
@@ -2,16 +2,59 @@
 #include<algorithm>
 #include<vector>
 #include<string>
+#include<queue>
+#include<unordered_set>
 
 using namespace std;
 
 class Solution 
 {
+    int n;
     string begin, end;
-    vector<string> wordList;
+    unordered_set<string> set;
+
+    int bfs()
+    {
+        if (set.find(end) == set.end()) return 0;
+
+        queue<pair<string, int>> q;
+
+        q.push({ begin, 1 });
+
+        while (q.size())
+        {
+            string now = q.front().first;
+            int cnt = q.front().second;
+            q.pop();
+
+            if (now == end) return cnt;
+
+            for (int i = 0; i < now.size(); i++)
+            {
+                char original = now[i];
+
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    if (c == original) continue;
+
+                    now[i] = c;
+
+                    if (set.count(now))
+                    {
+                        q.push({ now, cnt + 1 });
+                        set.erase(now);
+                    }
+                }
+
+                now[i] = original;
+            }
+        }
+
+        return 0;
+    }
 
 public:
-    Solution() : begin(""), end("")
+    Solution() : n(0), begin(""), end("")
     {
 
     }
@@ -20,7 +63,10 @@ public:
     {
         begin = move(beginWord);
         end = move(endWord);
-        wordList = move(input);
+        
+        set = unordered_set<string>(input.begin(), input.end());
+        
+        return bfs();
     }
 };
 
