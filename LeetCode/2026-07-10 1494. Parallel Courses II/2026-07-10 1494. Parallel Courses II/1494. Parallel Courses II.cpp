26e91763e504acbc7a573714ac7df45dc9f75a9e
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-07-10 17:27:50 +0900

    2026.07.10

---
+++ LeetCode/2026-07-10 1494. Parallel Courses II/2026-07-10 1494. Parallel Courses II/1494. Parallel Courses II.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    int minNumberOfSemesters(int n, vector<vector<int>>& relations, int k) 
+    {
+
+    }
+};
+
+int main()
+{
+
+	return 0;
+}
