83863e01f54deb2b890b260900c9185e5ef137db
KimJunSeo <108670965+k99812@users.noreply.github.com>
2026-07-14 17:24:19 +0900

    2026.07.14

---
+++ LeetCode/2026-07-14 1349. Maximum Students Taking Exam/2026-07-14 1349. Maximum Students Taking Exam/1349. Maximum Students Taking Exam.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    int maxStudents(vector<vector<char>>& seats) 
+    {
+
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
